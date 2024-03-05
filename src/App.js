475931aadef1d623fbe9085c9eaf38e50026429b
thiyada-kitt <111549728+thiyada-kitt@users.noreply.github.com>
2024-03-05 17:02:12 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ return (
+    <div className="App">
+      <header className="App-header">
+        <img src= "cat.png" className="App-logo" alt="logo" />
+        <p>
+          xxxx
+        </p>
+        <a>
+          xxxxx
+        </a>
+      </header>
+    </div>
+  );
  
 }
 
