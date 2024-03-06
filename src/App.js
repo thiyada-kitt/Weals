3afdd41c7285b0ace34c519f0b7636802bd24a1f
thiyada-kitt <111549728+thiyada-kitt@users.noreply.github.com>
2024-03-07 00:06:06 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+
 
 function App() {
   const [showContent, setShowContent] = useState(true);
