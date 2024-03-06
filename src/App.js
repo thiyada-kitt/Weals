422e3c169ec6d19e5cbcede29bbbc886dfadeeb6
thiyada-kitt <111549728+thiyada-kitt@users.noreply.github.com>
2024-03-06 21:30:38 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   const [showContent, setShowContent] = useState(true);
   const [opacity, setOpacity] = useState(0);
+  const [currentPage, setCurrentPage] = useState('NewPage1');
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -24,6 +25,10 @@ function App() {
     return () => clearInterval(interval);
   }, [showContent]);
 
+  const handleButtonClick = () => {
+    setCurrentPage('NewPage2');
+  };
+
   return (
     <div className="App">
       {showContent ? (
@@ -34,11 +39,21 @@ function App() {
           </p>
         </header>
       ) : (
-        <div className="NewPage1" style={{ display: 'flex', flexDirection: 'column' }}>
-          <p style={{ opacity, transition: 'opacity 1s ease-in-out' }}>🎂 Happy birthday kub bbe 🎉</p>
-          <div style={{ opacity, transition: 'opacity 1s ease-in-out', fontWeight: 'bold' }}>7 March 2024</div>
-          <br />
-          <button className="button" style={{ opacity, transition: 'opacity 1s ease-in-out' }}>♡</button>
+        <div className={currentPage} style={{ display: 'flex', flexDirection: 'column' }}>
+          {currentPage === 'NewPage1' && (
+            <>
+              <p style={{ opacity, transition: 'opacity 1s ease-in-out' }}>🎂 Happy birthday kub bbe 🎉</p>
+              <div style={{ opacity, transition: 'opacity 1s ease-in-out', fontWeight: 'bold' }}>7 March 2024</div>
+              <br />
+              <button className="button" style={{ opacity, transition: 'opacity 1s ease-in-out' }} onClick={handleButtonClick}>♡</button>
+            </>
+          )}
+        </div>
+      )}
+
+      {currentPage === 'NewPage2' && (
+        <div className="NewPage2">
+          <p>สุขสันต์วันครบรอบสองเดือน</p>
         </div>
       )}
     </div>
