62dca3f3c076bc1ce0dbfe93a21de446a4f3d64c
thiyada-kitt <111549728+thiyada-kitt@users.noreply.github.com>
2024-03-06 19:16:19 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
- return (
+  const [showContent, setShowContent] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowContent(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
     <div className="App">
-      <header className="App-header">
-        <img src= "cat.png" className="App-logo" alt="logo" />
-        <p>
-          Loading love...
-        </p>
-      </header>
+      {showContent ? (
+        <header className="App-header">
+          <img src="cat.png" className="App-logo" alt="logo" />
+          <p>
+            Loading love...
+          </p>
+        </header>
+      ) : (
+        <div className="NewPage">
+          <p>Happy birthday kub bbe</p>
+        </div>
+      )}
     </div>
   );
- 
 }
 
 export default App;
